eff6e7667f97326e8142ba22dd42dbc33ed02a92
lelikone777 <lelikone777@users.noreply.github.com>
2020-11-14 19:36:54 +0300

    1

--- front-page.php
+++ front-page.php
@@ -508,96 +508,73 @@ wp_reset_postdata(); // Сбрасываем $post
                 <ul class="career-grid">
                 <?php		
                     global $post;
-                    //формируем посты в базу данных
+
                     $query = new WP_Query( [
-                    //подключаем 3 поста
-                    'posts_per_page' => 3,
-                    'category' => 30,
+                        'posts_per_page' => 3,
+                        'category_name' => 'career',
                     ] );
-                    //проверяем, есть ли посты
+
                     if ( $query->have_posts() ) {
-                        // создаем переменную счетчик постов
+                        //создаем счетчик постов
                         $cnt = 0;
-                        //пока посты есть, выводим их
                         while ( $query->have_posts() ) {
-                        $query->the_post();
-                        // увеличиваем счетчик постов
+                            $query->the_post();
+                            //увеличиваем счетчик постов $cnt
                             $cnt++;
                             switch ($cnt) {
-                            //выводим первый пост
-                                case '1': ?>
+                                case '1':
+                                    ?>
+                                    <img src="<?php echo get_template_directory_uri().'/assets/images/career.png';?>" alt="career-img" class="career-grid-image"> 
                                     <li class="career-grid-item career-grid-item-1">
-                                        <span class="category-name"> <?php $category = get_the_category(); echo $category [0]->name; ?></span>
-                                        <h4 class="article-grid-title"> <?php echo mb_strimwidth(get_the_title(), 0, 50, '...')?></h4>
-                                        <p class="article-grid-excerpt"><?php echo mb_strimwidth(get_the_excerpt(), 0, 160, '...')?></p>
-                                        <div class="article-grid-info">
-                                            <div class="author">
-                                                <?php $author_id = get_the_author_meta('ID'); ?>
-                                                <img src="<?php echo get_avatar_url($author_id)?>" alt="" class="author-avatar">
-                                                <span class="author-name"> <strong> <?php the_author() ?></strong> :
-                                                    <?php the_author_meta('description') ?> </span>
-                                            </div>
-                                        </div>                                        
+                                        <?php 
+                                            foreach (get_the_category() as $category) {
+                                                printf(
+                                                    '<a href="%s" class="category-link %s">%s</a>',
+                                                    esc_url( get_category_link( $category ) ),
+                                                    esc_html( $category -> slug ),
+                                                    esc_html( $category -> name ),
+                                                );
+                                            }
+                                        ?>
+                                        <!-- <?php echo mb_strimwidth(get_the_title(), 0, 60, '...') ?> </h4>          -->
+                                        <h4><?php the_title()?></h4>
+                                        <p><?php echo mb_strimwidth(get_the_excerpt(), 0, 80, '...') ?></p>
+                                        <a href="<?php echo get_the_permalink(); ?>" class="more career-grid-button">Читать далее</a>
                                     </li>
                                     <?php
                                     break;
-                            //выводим второй пост
-                                case '2': ?>
-            <li class="article-grid-item article-grid-item-2">
-                <img src="<?php if( has_post_thumbnail() ) {
-                                    echo get_the_post_thumbnail_url();
-                                }
-                                else {
-                                    echo get_template_directory_uri().'/assets/images/img-default.png';
-                                }  ?>" alt="" class="article-grid-thumb">
-                <a href="<?php the_permalink()?>" class="article-grid-permalink">
-                    <span class="tag">
-                        <?php $posttags = get_the_tags();
-                                            if ($posttags) {
-                                                echo $posttags[0]->name . ' ';
-                                            } ?>
-                    </span>
-                    <span class="category-name"> <?php $category = get_the_category(); echo $category[0]->name; ?>
-                    </span>
-                    <h4 class="article-grid-title"> <?php the_title()?></h4>
-                    <div class="article-grid-info">
-                        <div class="author">
-                            <?php $author_id = get_the_author_meta('ID'); ?>
-                            <img src="<?php echo get_avatar_url($author_id)?>" alt="" class="author-avatar">
-                            <div class="author-info">
-                                <span class="author-name"> <strong> <?php the_author() ?></strong></span>
-                                <span class="date"><?php the_time( 'j F' );?></span>
-                                <div class="comments">
-                                    <svg width="19" height="15" class="icon comments-icon" fill="#BCBFC2">
-                                        <use
-                                            xlink:href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#Comment">
-                                        </use>
-                                    </svg>
-                                    <span class="comments-counter"> <?php comments_number('0', '1', '%')  ?> </span>
-                                </div>
-                                <div class="likes">
-                                    <svg width="19" height="15" class="icon likes-icon" fill="#BCBFC2">
-                                        <use
-                                            xlink:href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#heart">
-                                        </use>
-                                    </svg>
-                                    <span class="likes-counter"><?php comments_number('0', '1', '%')  ?></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </li>
-            <?php
-            break;
-            }  
-        }
-    } else {
-        // Постов не найдено
-    }
+                                case '2':
+                                    ?> 
+                                    <a class="career-grid-permalink" href="<?php echo get_the_permalink();?>">  
+                                    <li class="career-grid-item career-grid-item-2">
+                                        <h4><?php the_title()?></h4>
+                                        <p><?php echo mb_strimwidth(get_the_excerpt(), 0, 50, '...') ?></p>
+                                        <span class="article-date"><?php the_time( 'j F' );?></span>
+                                    </li>
 
-    wp_reset_postdata(); // Сбрасываем $post
-    ?> 
+                                    <?php
+                                break;
+                                case '3':
+                                    ?>
+
+                                    <a class="career-grid-permalink" href="<?php echo get_the_permalink();?>">  
+                                    <li class="career-grid-item career-grid-item-3">
+                                        <h4><?php the_title()?></h4>
+                                        <p><?php echo mb_strimwidth(get_the_excerpt(), 0, 50, '...') ?></p>
+                                        <span class="article-date"><?php the_time( 'j F' );?></span>
+                                    </li>
+                                    
+                                    <?php
+                                break;
+                            }
+                        }
+                    } else {
+                        // Постов не найдено
+                       ?> <p>Постов не найдено</p> <?php
+                    }
+
+                    wp_reset_postdata(); // Сбрасываем $post
+                ?>
             </div>
         </div>
     </div>
